e880e2353e77fba8c84cd411dd108392afe1a6bc
waqas rafiq <wm66179@gmail.com>
2023-03-24 00:34:01 +0500

    ok

---
+++ script.js
@@ -0,0 +1,7 @@
+function calculatePercentage() {
+    var grade = parseInt(document.getElementById("grade").value);
+    var percentage = grade + "%";
+  
+    document.getElementById("percentage").value = percentage;
+  }
+  
